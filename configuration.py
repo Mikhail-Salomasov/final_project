b3faa5f51958f950d9e8e999e40ec58e14bd740e
unknown <mikesal.edu@gmail.com>
2023-09-13 11:24:07 +0300

    My first commit

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 # Саломасов Михаил, 8-я когорта - Финальный проект. Инженер по тестированию плюс
-URL_SERVICE = "https://acd19d8f-cd2c-4fe1-b8ec-f7291519e88a.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://fc6459ad-22be-47ed-b4c2-5a9743f129c2.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_INFO = "/api/v1/orders/track"
